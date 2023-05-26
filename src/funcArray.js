1e19fd26bc0c040759b43ec950de1e2e8f0a405f
Angela-GM <angelagm0112@gmail.com>
2023-05-26 14:27:19 +0200

    complete

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,16 +1,99 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(numero1,numero2){}
-export function findLongestWord(array){}
-export function sumArray(array){}
-export function averageNumbers(array){}
-export function averageWordLength(){}
-export function uniquifyArray(){}
-export function doesWordExist(array, palabra){}
-export function howManyTimes(){}
-export function greatestProduct(){}
+export function maxOfTwoNumbers(numero1, numero2) {
+  let numMax = 0;
+  if (numero1 > numero2) {
+    numMax = numero1;
+  } else if (numero2 > numero1) {
+    numMax = numero2;
+  } else if (numero1 === numero2) {
+    numMax = numero1;
+  }
 
+  return numMax;
+}
+export function findLongestWord(array) {
+  let longestWord = "";
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (longestWord.length < array[i].length) {
+        longestWord = array[i];
+      } else if (longestWord.length === array[i].length) {
+        longestWord = longestWord;
+      }
+    }
+  }
 
+  return longestWord;
+}
+export function sumArray(array) {
+  const suma = array.reduce(
+    (acumulador, valorActual) => acumulador + valorActual,
+    0
+  );
+  return suma;
+}
+export function averageNumbers(array) {
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    const suma = array.reduce(
+      (acumulador, valorActual) => acumulador + valorActual
+    );
+    const total = suma / array.length;
+    return total;
+  }
+}
+export function averageWordLength(array) {
+  let sumWordLength = 0;
+  if (array.length === 0) {
+    return undefined;
+  } else if (array.length === 1) {
+    sumWordLength = array[0].length;
+  } else {
+    sumWordLength = array.join("").length;
+  }
 
+  return sumWordLength / array.length;
+}
+export function uniquifyArray(array) {
+  if (array.length === 0) {
+    return undefined;
+  } else if (array.length === 1) {
+    return array;
+  } else {
+    const uniqueArray = [];
 
+    for (let i = 0; i < array.length; i++) {
+      if (!uniqueArray.includes(array[i])) {
+        uniqueArray.push(array[i]);
+      }
+    }
 
+    return uniqueArray;
+  }
+}
+export function doesWordExist(array, palabra) {
+  let existWord = false;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === palabra) {
+      existWord = true;
+    }
+  }
+  return existWord;
+}
+export function howManyTimes(array, palabra) {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let sumTimes = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === palabra) {
+      sumTimes++;
+    }
+  }
+  return sumTimes;
+}
+export function greatestProduct() {}
