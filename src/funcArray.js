0591965d497d5334b07a6fdb0167ed6c34f20de3
AleNS <isalejanaranjo@gmail.com>
2023-09-06 22:32:43 +0200

    longestWord function done

--- src/funcArray.js
+++ src/funcArray.js
@@ -9,7 +9,20 @@ export function maxOfTwoNumbers(numero1,numero2){
         return numero1
     }
 }
-export function findLongestWord(array){}
+export function findLongestWord(array) {
+    if (array.length === 0) {
+        return undefined;
+    } else if (array.length === 1) {
+        return array[0];
+    } else {
+        return array.reduce((longestWord, currentWord) => {
+            if (currentWord.length > longestWord.length) {
+                return currentWord;
+            }
+            return longestWord;
+        }, array[0]);
+    }
+}
 export function sumArray(array){}
 export function averageNumbers(array){}
 export function averageWordLength(){}
