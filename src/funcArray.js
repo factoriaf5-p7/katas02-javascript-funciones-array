cdd177aeef1154abaf75d63ad9c0610d29b186fe
AleNS <isalejanaranjo@gmail.com>
2023-09-06 10:17:56 +0200

    First function done

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,6 +1,14 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(numero1,numero2){}
+export function maxOfTwoNumbers(numero1,numero2){
+    if (numero1 < numero2){
+        return numero2
+    } else if(numero2<numero1){
+        return numero1
+    } else if (numero1==numero2){
+        return numero1
+    }
+}
 export function findLongestWord(array){}
 export function sumArray(array){}
 export function averageNumbers(array){}
